f4996f6b71b5c1dfeb0729095602b2fbaa514344
Jerome A <jeromealapad13@gmail.com>
2024-11-27 13:18:40 +0800

    add homepage and recipelist

---
+++ src/components/pages/HomePage.vue
@@ -0,0 +1,21 @@
+<script setup>
+import RecipeList from '../recipe/RecipeList.vue';
+</script>
+
+<template>
+    <div class="container-md my-5 py-5">
+        <div class="recipe__title text-center">
+            <h2>All Recipe</h2>
+            <p style="font-size: 15px">
+            Find and share everyday cooking inspiration on
+            Allrecipes.Discover recipes, cooks, videos, and
+            how-tos based on the food you love and the friends
+            you follow.
+            <RecipeList />
+            </p>
+        </div>
+    </div>
+</template>
+
+<style>
+</style>
