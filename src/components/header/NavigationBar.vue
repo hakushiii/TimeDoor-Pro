da3e82fbce3c6b02b25e955c51f1e858af4013e9
Jerome A <jeromealapad13@gmail.com>
2024-11-27 19:56:53 +0800

    dynamic component

--- src/components/header/NavigationBar.vue
+++ src/components/header/NavigationBar.vue
@@ -1,6 +1,37 @@
 <script setup>
-import SearchMenu from './SearchMenu.vue';
-import SignupMenu from './SignupMenu.vue';
+    import SearchMenu from './SearchMenu.vue';
+    import SignupMenu from './SignupMenu.vue';
+    import ProfileMenu from './ProfileMenu.vue';
+
+    import { computed, ref, watch } from 'vue';
+    import { useStore } from 'vuex';
+
+    const menuComponent = ref("signup-menu")
+    const store = useStore();
+
+    const components  ={
+        'signup-menu': SignupMenu,
+        'profile-menu': ProfileMenu,
+    }
+
+    const getToken = computed(() => {
+        return store.state.auth.token
+    })
+
+    if (!getToken.value) {
+        menuComponent.value = "signup-menu"
+    } else {
+        menuComponent.value = "profile-menu"
+    }
+
+    watch(getToken, (newValue, oldValue) => {
+        if (!newValue) {
+            menuComponent.value = "signup-menu"
+        } else {
+            menuComponent.value = "profile-menu"
+        }
+    })
+
 </script>
 
 <template>
@@ -9,6 +40,6 @@ import SignupMenu from './SignupMenu.vue';
         justify-content-between align-items-center"
         style="width: 450px">
         <SearchMenu />
-        <SignupMenu />
+        <component :is="components[menuComponent]" />
     </div>
 </template>
